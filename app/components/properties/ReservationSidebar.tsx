0c51a8d236958eef37be8f3fc6c1a04842547661
PrajwalFewaliRana <prajwalmagar68@gmail.com>
2026-01-28 23:18:17 +0545

    disabling booked dates

--- app/components/properties/ReservationSidebar.tsx
+++ app/components/properties/ReservationSidebar.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState,useEffect } from "react";
 import {Range} from 'react-date-range'
-import {differenceInDays,eachDayOfInterval} from "date-fns";
+import {differenceInDays,eachDayOfInterval,format} from "date-fns";
 
 import apiService from "@/app/services/apiService";
 import useLoginModel from "@/app/hooks/useLoginModel";
@@ -30,8 +30,30 @@ const ReservationSidebar:React.FC<ReservationSidebarProps> = ({property,userId})
   const [totalPrice,setTotalPrice] = useState<number>(1);
   const [dateRange,setDateRange] = useState<Range>(initialDateRange);
   const [minDate,setMinDate] = useState<Date>(new Date());
+  const [bookedDates,setBookedDates]=useState<Date[]>([])
   const [guests,setGuests] = useState<string>('1');
   const guestsRange = Array.from({length:property.guests},(_,index)=>index+1)
+
+  const performBooking = async ()=>{
+
+    if (userId){
+      if (dateRange.startDate && dateRange.endDate){
+        const formData = new FormData();
+        formData.append('guests',guests);
+        formData.append('start_date',format(dateRange.startDate,'yyyy-MM-dd'))
+        formData.append('end_date',format(dateRange.endDate,'yyyy-MM-dd'))
+        formData.append('number_of_nights',nights.toString())
+        formData.append('total_price',totalPrice.toString())
+        const response = await apiService.postForm(`/api/properties/${property.id}/book/`,formData)
+
+        if (response.success){
+          console.log('Booking successful')
+        }else{
+          console.log('Something went wrong...')
+        }
+      }
+    }
+  }
 
   const _setDateRange =(selection:any)=>{
     const newStartDate = new Date(selection.startDate);
@@ -48,7 +70,22 @@ const ReservationSidebar:React.FC<ReservationSidebarProps> = ({property,userId})
     })
   }
 
+  const getReservations = async () =>{
+    const reservations = await apiService.get(`/api/properties/${property.id}/reservations/`)
+    let dates: Date[] = []
+
+    reservations.forEach((reservation:any) => {
+      const range = eachDayOfInterval({
+        start:new Date(reservation.start_date),
+        end:new Date(reservation.end_date)
+      })
+      dates= [...dates,...range]
+    });
+    setBookedDates(dates)
+  }
+
   useEffect(()=>{
+    getReservations()
     if(dateRange.startDate && dateRange.endDate){
       const dayCount = differenceInDays(
         dateRange.endDate,
@@ -71,7 +108,7 @@ const ReservationSidebar:React.FC<ReservationSidebarProps> = ({property,userId})
   return (
     <aside className="mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
       <h2 className="mb-5 text-2xl">${property.price_per_night} per night</h2>
-      <DatePicker value={dateRange} onChange={(value)=>_setDateRange(value.selection)} />
+      <DatePicker bookedDates={bookedDates} value={dateRange} onChange={(value)=>_setDateRange(value.selection)} />
       <div className="mb-6 p-3 border border-gray-400 rounded-xl">
         <label htmlFor="" className="mb-2 font-bold block text-xs">
           Guests
@@ -82,7 +119,7 @@ const ReservationSidebar:React.FC<ReservationSidebarProps> = ({property,userId})
           ))}
         </select>
       </div>
-      <div className="w-full mb-6 py-6 text-center text-white  airbnb rounded-xl">
+      <div onClick={performBooking} className="w-full mb-6 py-6 text-center text-white  airbnb rounded-xl">
         Book
       </div>
       <div className="mb-4 flex justify-between align-center">
