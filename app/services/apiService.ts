329b8996597b2a2c569e3714918ac10eaf60b07c
PrajwalFewaliRana <prajwalmagar68@gmail.com>
2026-02-09 01:02:46 +0545

    some bug solved new bug arrived

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -3,25 +3,50 @@ import { getAccessToken } from "../lib/actions";
 const apiService = {
   get: async function (url: string): Promise<any> {
     // console.log("get", url);
-    // const token = await getAccessToken();
-    return new Promise((resolve, reject) => {
-      fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          // Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((response) => response.json())
-        .then((json) => {
-          // console.log("Response", json);
-          resolve(json);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
+    const token = await getAccessToken();
+    const headers:HeadersInit ={
+      Accept:'application/json',
+      'Content-Type':'application/json',
+    }
+    if(token){
+      headers["Authorization"]=`Bearer ${token}`;
+    }
+
+    const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_HOST}${url}`,
+    {
+      method: "GET",
+      headers,
+    }
+  );
+
+  if (!response.ok) {
+    const text = await response.text();
+    throw new Error(text || "Request failed");
+  }
+
+  return await response.json();
+
+
+
+    // return new Promise((resolve, reject) => {
+    //   fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+    //     method: "GET",
+    //     headers: {
+    //       Accept: "application/json",
+    //       "Content-Type": "application/json",
+    //       // Authorization: `Bearer ${token}`,
+    //     },
+    //   })
+    //     .then((response) => response.json())
+    //     .then((json) => {
+    //       // console.log("Response", json);
+    //       resolve(json);
+    //     })
+    //     .catch((error) => {
+    //       reject(error);
+    //     });
+    // });
   },
   post: async function (url: string, data: any): Promise<any> {
     // console.log("post", url, data);
--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images:{
+    // domains:['127.0.0.1','localhost'],
     remotePatterns:[
       {
         protocol:'http',
